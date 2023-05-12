a96237561018ada2e5b54349cad7b17dbecac1f1
子園 <133328000+cerisecerise@users.noreply.github.com>
2023-05-12 16:36:30 +0800

    Add files via upload

---
+++ p作業一.py
@@ -0,0 +1,29 @@
+#message="我是貓咪"
+message="我是teddy bear"
+print(message)
+
+number=[1,2,3,4,5,6,7,8,9,10]
+total=sum(number)
+print(total)
+
+message="I hate monday!"
+print("文字串長度為",len(message))
+
+number=[2,5,9,11,24,16,21]
+print("列表中最大數字為",max(number))
+print("列中最小數字為",min(number))
+
+n=5
+for i in range(1,n+1):
+    print("1到n的平方數為",i*i)
+    
+number=[1,2,4,6,8,8,8,9,10,11]
+print("8出現的次數為:%d"% number.count(8))
+    
+pets=["dog","cat","snake","dog","dog","rabbit","mouse"]
+print("狗出現的次數為:%d"% pets.count("dog"))
+
+
+    
+    
+    
